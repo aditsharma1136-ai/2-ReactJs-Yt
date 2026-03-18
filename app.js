5d2022a06b8bdeb45381dae545d892622e19af98
Aditya Kr Sharma <adit.sharma.1136@gmail.com>
2026-03-18 21:44:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// Types of export - Default , Named
+// 1. Default. : It only export one element at a time but we can import this name with anything. 
+// const arr = [20, 7 , 8 , 25]
+
+// export default arr
+
+// 2. Named Export. : It will help to export particular element by but we have to import it by it's orignal name.
+// export var user = 'Aditya'
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+var name = 'Pallab';
+export const age = 19;
+export var skills = ['Frontend', 'Backend', 'UI/UX'];
+
+export default name
---
+++ script.js
@@ -0,0 +1,14 @@
+// Default:
+// import array from './app.js'
+
+// console.log(array);
+// Named:
+// import { user } from "./app.js";
+
+// console.log(user);
+// -------------------------------------------------------------------------------------------------------------------------------------
+import { skills , age } from "./app.js";
+console.log(skills , age);
+
+import naam from './app.js';
+console.log(naam);
