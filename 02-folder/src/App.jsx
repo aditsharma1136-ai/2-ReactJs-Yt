f21ce097921b8d6ab06f92ea49134dd9e9a776d1
Aditya Kr Sharma <adit.sharma.1136@gmail.com>
2026-03-18 22:54:58 +0530

    new

---
+++ 02-folder/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./assets/components/Header"
+import Navbar from "./assets/components/Navbar"
+import Card from "./assets/components/card" 
+
+const App = () => {
+  return (
+    <div>
+      <Card />
+      <Navbar />
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ 02-folder/src/assets/components/card.jsx
@@ -0,0 +1,12 @@
+const card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Aditya kr Sharma</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt, provident.</p>
+      </div>
+    </div>
+  )
+}
+
+export default card
